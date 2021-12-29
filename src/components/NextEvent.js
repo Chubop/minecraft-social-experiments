23f13690da3eb725b525e5118b7458478ee94739
Chubop <samuelokirby@gmail.com>
2021-12-29 17:23:20 -0500

    changed NextEvent

--- src/components/NextEvent.js
+++ src/components/NextEvent.js
@@ -7,15 +7,16 @@ function NextEvent() {
     <div style={{textAlign: 'center'}}>
       <div style={{marginTop: '5vh'}}>
         <Typography style={{backgroundColor: 'rgba(0, 0, 0, 0.06', paddingBottom: 20}}>
-            Our first experiment will be set between five warring teams with two goals: 
+            Our first experiment titled <span style={{color: 'darkred'}}>Doom</span> will be set between five warring teams with two goals: 
             <span style={{color: 'purple'}}> to survive an unknown impending threat,</span> and to <span style={{color: 'purple'}}>come out on top.</span>
         </Typography>
       </div>
       <span>
           <Typography style={{ marginTop: '5vh' }}>
-            <Typography style={{ marginBottom: '2vh', color: 'black', fontSize: '5vw', fontFamily: 'MinecraftFive', lineHeight: 1 }}>
+            <Typography style={{ marginBottom: '2vh', color: 'black', fontSize: '5vw', fontFamily: 'MinecraftFive', lineHeight: 1}}>
               <div style={{ textDecoration: 'underline' }}>Event Date</div>
               <span style={{ fontFamily: 'MinecraftFive', fontWeight: 'bold', color: 'dimGray' }}>1 / 2 / 2022</span>
+              <div style={{color: 'dimgray'}}>4:00 pm est</div>
             </Typography>
               <Countdown style={{marginTop: '5vh'}} date={"02 Jan 2022 16:00:00 EST"}/>
           </Typography>
